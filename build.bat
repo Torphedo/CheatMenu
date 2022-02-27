3d0453ebf4a0ca42fe39e5e5739d766f5477415a
Torphedo <ahiatt.cosgrove@gmail.com>
2022-02-26 19:28:52 -0500

    Move mod files to src\

--- build.bat
+++ build.bat
@@ -1,4 +1,3 @@
-@echo off
-echo Creating BNP...
-echo.
-src\bin\7z.exe a CheatMenu.bnp info.json content logs
+if exist CheatMenu.bnp del CheatMenu.bnp
+cd src
+..\assets\bin\7z.exe a ..\CheatMenu.bnp info.json content logs
